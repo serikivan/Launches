4fa14c40ab86d62ee62252854cd21de52bd36367
megatron <130218949+serikivan@users.noreply.github.com>
2024-12-08 19:24:44 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import NavbarComponent from './components/NavigationBar/NavigationBar'
-import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Launches">
       <NavbarComponent />
       <App />
-    </BrowserRouter>
   </React.StrictMode>,
 )
